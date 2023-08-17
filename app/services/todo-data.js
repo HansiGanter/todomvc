b726e867befcc83dd5f90ee08bf0629b205a64d1
Hans Ganter <hansi.ganter@gmx.at>
2023-08-18 00:17:30 +0800

    finish todomvc ember

---
+++ app/services/todo-data.js
@@ -0,0 +1,46 @@
+import Service from '@ember/service';
+import { tracked } from "@glimmer/tracking";
+import { action } from "@ember/object";
+
+class Todo {
+    @tracked text = "";
+    @tracked isCompleted = false;
+
+    constructor(text) {
+        this.text = text;
+    }
+}
+
+export default class TodoDataService extends Service {
+    @tracked todos = [];
+
+    get all() {
+        return this.todos;
+    }
+
+    get incomplete() {
+        return this.todos.filter(t => !t.isCompleted);
+    }
+
+    get completed() {
+        return this.todos.filter(t => t.isCompleted);
+    }
+
+    @action
+    add(text) {
+        let newTodo = new Todo(text);
+
+        this.todos = [...this.todos, newTodo];
+    }
+
+    @action
+    clearCompleted() {
+        this.todos = this.incomplete;
+    }
+
+    @action
+    toggleCompletion(todo) {
+        todo.isCompleted = !todo.isCompleted;
+    }
+}
+
